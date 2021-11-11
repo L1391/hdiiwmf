4c0177cd80064229e78cb1c0e713060bf0e79c1e
Lindsey <33672165+L1391@users.noreply.github.com>
2021-11-10 21:20:07 -0500

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,44 @@
+const rad = 125;
+
+function setup() {
+    var canvas = createCanvas(400,400);
+    canvas.parent("canvas-holder");
+} 
+
+function draw() {
+}
+
+function updateTree() {
+    background(255);
+
+    var weekNum = document.getElementById("timeline").value;
+    var interactionSum = getWeeklyTotal(weekNum);
+
+    text("Week: " + weekNum, 10, 20);
+    text("Total interactions: " + interactionSum, 10, 40);
+
+    var i = 1;
+    for (var key in thisWeek) {
+        stroke(random(255), random(255), random(255));
+
+        var x = rad*cos(i*TWO_PI/Object.keys(thisWeek).length);
+        var y = rad*sin(i*TWO_PI/Object.keys(thisWeek).length);
+
+        strokeWeight(1);
+        circle(width/2 + x, height/2 + y, 10);
+        text(key, width/2 + x + 10, height/2 + y + 5)
+        let v = createVector(x, y);
+        if (typeof data["interactions"][weekNum][key] === 'undefined') {
+            v.mult(0);
+        } else {
+            v.mult(data["interactions"][weekNum][key]/interactionSum);
+        }
+
+        strokeWeight(5);
+        line(width/2, height/2, width/2 + v.x, height/2 + v.y);
+
+        i++;
+    }
+
+
+}
---
+++ index.js
@@ -0,0 +1,127 @@
+let data;
+let thisWeek = {};
+
+window.onload = () => {
+    document.getElementById("add-week").addEventListener("click", (e) => {
+        var newWeek ={};
+        for (var key in thisWeek) {
+            newWeek[key] = document.getElementById(key).value;
+        }
+
+        console.log(data);
+        data["interactions"].push(newWeek);
+        console.log(data);
+
+        localStorage.setItem('friend-data', JSON.stringify(data));
+        console.log(localStorage.getItem('friend-data'));
+
+        getStorage();
+        reloadTimeline();
+        updateTree();
+    });
+
+    document.getElementById("update-week").addEventListener("click", (e) => {
+        for (var key in thisWeek) {
+            thisWeek[key] = document.getElementById(key).value;
+        }
+
+        data["interactions"][data["interactions"].length -1] = thisWeek;
+
+        localStorage.setItem('friend-data', JSON.stringify(data));
+        console.log(localStorage.getItem('friend-data'));
+
+        getStorage();
+        updateTree();
+    });
+
+    document.getElementById("clear-nums").addEventListener("click", (e) => {
+        for (var key in thisWeek) {
+            thisWeek[key] = 0;
+        }
+
+        reloadRightControls();
+    });
+
+    document.getElementById("add-friend").addEventListener("click", (e) => {
+        var friendName = document.getElementById("add-friend-name").value;
+        thisWeek[friendName] = 0;
+        reloadLeftControls();
+        reloadRightControls();
+        updateTree();
+
+    });
+
+    document.getElementById("timeline").addEventListener("change", (e) => {
+        updateTree();
+    });
+
+   getStorage();
+    
+    reloadTimeline();
+
+    reloadLeftControls();
+
+    reloadRightControls();
+    
+    updateTree();
+}
+
+function getStorage() {
+    data = JSON.parse(localStorage.getItem('friend-data'));
+    //localStorage.clear();
+
+    if(data === null) {
+        data = {'interactions': [{}]};
+    } 
+
+    thisWeek = data["interactions"][data["interactions"].length -1];
+}
+
+function getWeeklyTotal(weekNum) {
+    var interactionSum = 0;
+    for (var key in data["interactions"][weekNum]) {
+        interactionSum += int(data["interactions"][weekNum][key]);
+    }
+
+    return interactionSum;
+}
+
+function reloadTimeline() {
+    var timeline = document.getElementById("timeline");
+    timeline.min = 0;
+    timeline.max = data["interactions"].length - 1;
+
+}
+
+function reloadLeftControls() {
+    var friendList = document.getElementById("friend-list");
+    friendList.innerHTML = "";
+
+    for (var key in thisWeek) {
+        var friendItem = document.createElement("p");
+        friendItem.innerText = key;
+
+        friendList.append(friendItem);
+    }
+}
+
+function reloadRightControls() {
+    var interactionList = document.getElementById("interaction-list");
+    interactionList.innerHTML = "";
+
+    for (var key in thisWeek) {
+        var friendItem = document.createElement("div");
+        var friendItemName = document.createElement("p");
+        friendItemName.innerText = key;
+        var friendItemInteractions = document.createElement("input");
+        friendItemInteractions.setAttribute("type", "number");
+        friendItemInteractions.id = key;
+        friendItemInteractions.min = 0;
+        friendItemInteractions.value = thisWeek[key];
+
+        friendItem.append(friendItemName, friendItemInteractions);
+        
+        interactionList.append(friendItem);
+    }
+
+}
